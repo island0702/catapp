09cf8a4a6db5a893398caa75149c366a5c9c1d45
island0702 <126866343+island0702@users.noreply.github.com>
2023-04-28 16:22:27 +0900

    投稿画面修正２

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
   validates :image, presence: true
   validates :description, presence: true
 
   belongs_to :user
   has_one_attached :image
 
-  validates unless: :was_attached?
+  validates :title, presence: true, unless: :was_attached?
+
 
   def was_attached?
     self.image.attached?
